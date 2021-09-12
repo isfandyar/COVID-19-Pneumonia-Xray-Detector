2804850cc08fa308a16023a50210cac0dbf90521
isfandyar <isfandyar.virani@gmail.com>
2021-09-12 00:47:52 -0400

    Update imgrec_webapp.py

--- imgrec_webapp.py
+++ imgrec_webapp.py
@@ -12,6 +12,45 @@ import matplotlib.pyplot as plt
 import tensorflow as tf 
 import numpy as np 
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import string
+import nltk
+import warnings
+warnings.filterwarnings("ignore")
+import os
+import shutil
+import matplotlib.pyplot as plt
+%matplotlib inline
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.callbacks import EarlyStopping
+
+import pickle
+import keras
+from keras.layers import *
+from keras.models import *
+from keras.preprocessing import image
+
+
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import array_to_img
+
+import tensorflow as tf
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator,array_to_img
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Flatten
+
 print("Loading model") 
 global sess
 sess = tf.Session()
@@ -35,24 +74,35 @@ def prediction(filename):
     #Step 1
     my_image = plt.imread(os.path.join('uploads', filename))
     #Step 2
-    my_image_re = resize(my_image, (32,32,3))
+    my_image_re = resize(my_image, (224,224))
+    x = image.img_to_array(my_image_re)
+    x = np.expand_dims(x, axis =0)
+    images = np.vstack([x])
+    classes = model.predict(images, batch_size = 32)
+    predictedClass = np.argmax(classes, axis=1)
+    predictedClass = np.vectorize(dict.get)(predictedClass)
     
+    dict = {0: 'COVID-19 PNEUMONIA', 
+        1: 'NORMAL', 
+        2: 'OTHER PNEUMONIA'}
+
     #Step 3
     with graph.as_default():
       set_session(sess)
-      probabilities = model.predict(np.array( [my_image_re,] ))[0,:]
-      print(probabilities)
+      classes = model.predict(images, batch_size = 32)
+      predictedClass = np.argmax(classes, axis=1)
+      predictedClass = np.vectorize(dict.get)(predictedClass)
+      print(classes)
       #Step 4
-      number_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 
-'truck']
-      index = np.argsort(probabilities)
+      number_to_class = ['COVID-19 PNEUMONIA', 'NORMAL', 'OTHER PNEUMONIA']
+      index = np.argsort(classes)
       predictions = {
         "class1":number_to_class[index[9]],
         "class2":number_to_class[index[8]],
         "class3":number_to_class[index[7]],
-        "prob1":probabilities[index[9]],
-        "prob2":probabilities[index[8]],
-        "prob3":probabilities[index[7]],
+        "prob1":classes[index[9]],
+        "prob2":classes[index[8]],
+        "prob3":classes[index[7]],
       }
     #Step 5
     return render_template('predict.html', predictions=predictions)
